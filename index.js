df2bb9e3f3c9b3f5a199485d8fccf9cdd9e112d9
Parvizkh8 <33002107+Parvizkh8@users.noreply.github.com>
2021-01-23 20:04:24 +0100

    Upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomNumber1 = Math.random ();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1
+
+var randomImagesSouce = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src" , randomImagesSouce);
+//We can probably use the code below instead of the two last lines of above://
+//document.querySelectorAll("img")[0].setAttribute("src" , randomImagesSouce);//
+
+
+
+var randomNumber2 = Math.random ();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1
+
+var randomImagesSouce2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src" , randomImagesSouce2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "❤️ Player 1 wins!";
+}
+    else if (randomNumber1 < randomNumber2) {
+            document.querySelector("h1").innerHTML = "Player 2 wins! ❤️";
+    }
+    else {
+        document.querySelector("h1").innerHTML = "Draw!";
+    }
